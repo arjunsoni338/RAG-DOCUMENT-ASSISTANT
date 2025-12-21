c09e45c08e35e6498cdcaec55e22e56eab2999aa
Arjun Soni <arjun.official338@gmail.com>
2025-12-22 01:14:45 +0530

    Update compare_embeddings.py

--- compare_embeddings.py
+++ compare_embeddings.py
@@ -1,8 +1,16 @@
-from langchain_openai import OpenAIEmbeddings
-from langchain.evaluation import load_evaluator
+from langchain_openai import OpenAIEmbeddings 
+#LangChain is a Python/JavaScript framework that helps you to build applications using LLMs
+#Embeddings in OpenAI are numeric vector representations of text.
+#They convert words, sentences, or documents into lists of numbers so that a computer can understand their meaning,
+#similarity, and relationships.
+#In simple terms:
+#Embeddings = Text → Numbers that capture meaning.
+from langchain_classic.evaluation import load_evaluator
+# laod_evaluator is a helper that gives you built-in evaluation tools like embedding distance, etc.
 from dotenv import load_dotenv
+#Reads your .env (enviornment variable) file so you don’t hardcode your secret keys in code
 import openai
-import os
+import os #To access environment variables like OPENAI_API_KEY.
 
 # Load environment variables. Assumes that project contains .env file with API keys
 load_dotenv()
@@ -11,17 +19,26 @@ load_dotenv()
 # your .env file.
 openai.api_key = os.environ['OPENAI_API_KEY']
 
-def main():
-    # Get embedding for a word.
+def main(): # this is our main function like int main in c++
+        #   Get embedding for a word.
     embedding_function = OpenAIEmbeddings()
+        #   Here you create an embeddings generator.
+        #    Now this object can take in text like "apple" and return a giant vector, e.g. [0.01, -0.12, ...].
     vector = embedding_function.embed_query("apple")
+        #   embed_query("apple"): Sends the word “apple” to OpenAI’s embedding model.
     print(f"Vector for 'apple': {vector}")
     print(f"Vector length: {len(vector)}")
 
+        #   You’ve basically done:
+
+        #   “Hey model, what is ‘apple’ in your math language?”
+
     # Compare vector of two words
-    evaluator = load_evaluator("pairwise_embedding_distance")
-    words = ("apple", "iphone")
+    evaluator = load_evaluator("pairwise_embedding_distance") #You load an evaluator that calculates distance between embeddings.
+    words = ("apple", "iphone") #small tuple/row of words
     x = evaluator.evaluate_string_pairs(prediction=words[0], prediction_b=words[1])
+        #It internally embeds both strings and computes how far apart they are.
+        #Smaller distance = more similar in meaning.
     print(f"Comparing ({words[0]}, {words[1]}): {x}")
 
 
